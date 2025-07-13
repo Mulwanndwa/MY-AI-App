e7c01e4bda5c509a629c1be52eed772fc35662f8
Mpho Mulwanndwa <mulwanndwa.mpho@gmail.com>
2025-07-13 08:35:20 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,6 +15,51 @@ var myApp = new Vue({
      
     },
     methods:{
+        donate: function(){
+            const popupSwipe = app.popup.create({
+                el: '.paypal-popup',
+                swipeToClose: true,
+            });
+            popupSwipe.open()
+            paypal.Buttons().render('#paypal-button-container');
+        },
+        scanForResult: function(){
+            cordova.plugins.email.open({
+                to:      '',
+                cc:      '',
+                subject: 'Open Ai',
+                body:    myApp.response,
+                isHtml: true
+            });
+        },
+        scanForResult: function(){
+            cordova.plugins.barcodeScanner.scan(
+              function (result) {
+                  // alert("We got a barcode\n" +
+                  //       "Result: " + result.text + "\n" +
+                  //       "Format: " + result.format + "\n" +
+                  //       "Cancelled: " + result.cancelled);
+                  myApp.ai_input = result.text
+                  myApp.get_response()
+              },
+              function (error) {
+                  app.dialog.alert("Scanning failed: " + error);
+              },
+              {
+                  preferFrontCamera : true, // iOS and Android
+                  showFlipCameraButton : true, // iOS and Android
+                  showTorchButton : true, // iOS and Android
+                  torchOn: true, // Android, launch with the torch switched on (if available)
+                  saveHistory: true, // Android, save scan history (default false)
+                  prompt : "Place a barcode inside the scan area", // Android
+                  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                  formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+                  orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+                  disableAnimations : true, // iOS
+                  disableSuccessBeep: false // iOS and Android
+              }
+           );
+        },
         downloadResponse: function(){
             const element = $('.response').html();
             cordova.plugins.html2pdf.create(
